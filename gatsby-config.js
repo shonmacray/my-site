e2b3a22912565f4e3f8a9dc6ebac4533c47a2054
shon macray <shonmacray@gmail.com>
2020-07-08 10:29:54 +0300

    changed static data

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,9 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Shon Macray`,
+    description: `Hello, I am Shon, A creative ReactJS and React Native Rockstar
+    Developer`,
+    author: `shon macray`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -18,8 +19,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `Shon Macray`,
+        short_name: `shon macray`,
         start_url: `/`,
         background_color: `#663399`,
         theme_color: `#663399`,
@@ -28,6 +29,7 @@ module.exports = {
       },
     },
     `gatsby-plugin-smoothscroll`,
+
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
